bec326cddfbf8d6c0e1cfbdd56c45cc5dd1137b5
megagig <megagigsoftwaresolutions@gmail.com>
2023-07-17 21:31:08 +0100

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(id integer NOT NULL default nextval('seq_id'),
+ name varchar(100),
+ date_of_birth date,
+ escape_attempts int,
+ neutered boolean,
+ weight_kg float,
+ primary key(id));
+ 
