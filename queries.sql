76eb602da45b06ba29c42f50d0d117060819f513
megagig <megagigsoftwaresolutions@gmail.com>
2023-07-19 10:48:45 +0100

    Savepointn

--- queries.sql
+++ queries.sql
@@ -26,3 +26,15 @@ SELECT * FROM animals;
 --Update the species colunm
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+--deleting all Records
+BEGIN;
+DELETE FROM animals;
+
+--Rollback the transaction
+ROLLBACK;
+
+DELETE FROM animals WHERE date_of_birth > 'Jan 1, 2022';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
