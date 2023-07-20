6b8f40055ec6a79127f9a76270a310031f540be7
megagig <megagigsoftwaresolutions@gmail.com>
2023-07-20 13:30:48 +0100

    Owners of the most animals

--- queries.sql
+++ queries.sql
@@ -143,3 +143,10 @@ SELECT a.name
 FROM animals a
 JOIN owners o ON a.owner_id = o.id
 WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts=0;
+
+SELECT o.full_name, COUNT(a.id) AS animal_count
+FROM owners o
+JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name
+ORDER BY COUNT(a.id) DESC
+LIMIT 1;
